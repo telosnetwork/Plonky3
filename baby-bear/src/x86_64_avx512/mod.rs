cf7bd91c210a6f171000f2ab52b575149616b307
Paolo Tagliaferri <paolo1.tagliaferri@gmail.com>
2025-03-05 18:38:55 +0100

    Added compile-time check for AVX-512
    A Rust toolchain version not older than `nightly-2024-10-29` is required to build Plonky3 with the AVX-512 optimizations due to the fix introduced with `4478509fac9447f9ea70e8ac540aa9771897fc75`.

--- baby-bear/src/x86_64_avx512/mod.rs
+++ baby-bear/src/x86_64_avx512/mod.rs
@@ -1,3 +1,9 @@
+// Compile-time check to force the use of a nightly toolchain from 2024-10-29 or later.
+#[rustversion::before(2024-10-28)]
+fn compile_time_version_check() {
+    compile_error!("AVX-512 requires a nightly toolchain from 2024-10-29 or later");
+}
+
 mod packing;
 mod poseidon2;
 
